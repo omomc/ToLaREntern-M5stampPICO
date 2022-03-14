37d7fa7db1fa0dc45ad24a640b6123ebfe72f311
omomc <101538993+omomc@users.noreply.github.com>
2022-03-14 18:58:57 +0900

    Create tolarentern.h

---
+++ src/tolarentern.h
@@ -0,0 +1,14 @@
+#ifndef TOLARENTERN_H
+#define TOLARENTERN_H
+
+#define LANTERN_PIN 33
+#define LED_PIN 32
+
+void lantern_on();
+void lantern_off();
+void led_on();
+void led_off();
+void blinking_lantern(void* arg);
+void blinking_led(void* arg);
+
+#endif
