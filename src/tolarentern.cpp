1d8ca0322a3b0912fbc7465e15382d00990904ca
omomc <101538993+omomc@users.noreply.github.com>
2022-03-18 18:01:16 +0900

    Update tolarentern.cpp

--- src/tolarentern.cpp
+++ src/tolarentern.cpp
@@ -15,23 +15,3 @@ void led_on(){
 void led_off(){
   digitalWrite(LANTERN_PIN, LOW);
 }
-
-void blinking_lantern(void* arg){
-  int blinking_time_msec = 500; // msec
-  while (1) {
-    led_on();
-    vTaskDelay(blinking_time_usec);
-    led_off();
-    vTaskDelay(blinking_time_usec);
-  }
-}
-
-void blinking_led(void* arg){
-  int blinking_time_usec = 1000; // msec
-  while (1) {
-    lantern_on();
-    vTaskDelay(blinking_time_usec);
-    lantern_off();
-    vTaskDelay(blinking_time_usec);
-  }
-}
