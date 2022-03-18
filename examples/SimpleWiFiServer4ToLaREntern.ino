5ddba7bc0e5e722d5fafc09fa6ef93f409999190
omomc <101538993+omomc@users.noreply.github.com>
2022-03-18 18:16:15 +0900

    Update SimpleWiFiServer4ToLaREntern.ino

--- examples/SimpleWiFiServer4ToLaREntern.ino
+++ examples/SimpleWiFiServer4ToLaREntern.ino
@@ -1,6 +1,6 @@
 /*
- WiFi Web Server LED Blink
- ToLaREnternをWEBから制御するためのプログラムとなっています。
+ WiFi Web Server ToLaREntern Blink
+ OMOMCで販売するToLaREnternをWEBから制御するためのプログラムとなっています。
  
  A simple web server that lets you blink an LED via the web.
  This sketch will print the IP address of your WiFi Shield (once connected)
@@ -22,12 +22,26 @@ ported for sparkfun esp32
  */
 
 #include <WiFi.h>
-#include "tolarentenrn.h"
+
+#define LANTERN_PIN 33
+#define LED_PIN 32
 
 const char* ssid     = "yourssid";
 const char* password = "yourpasswd";
 
 WiFiServer server(80);
+
+void lantern_on(){
+    digitalWrite(LANTERN_PIN, LOW);
+}
+
+void lantern_off(){
+    digitalWrite(LANTERN_PIN, HIGH);
+}
+
+void led_on(){
+    digitalWrite(LANTERN_PIN, HIGH);
+}
 
 void setup()
 {
@@ -61,55 +75,53 @@ void setup()
 
 }
 
-int value = 0;
-
 void loop(){
- WiFiClient client = server.available();   // listen for incoming clients
+    WiFiClient client = server.available();   // listen for incoming clients
 
-  if (client) {                             // if you get a client,
-    Serial.println("New Client.");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
+    if (client) {                             // if you get a client,
+        Serial.println("New Client.");           // print a message out the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+        while (client.connected()) {            // loop while the client's connected
+            if (client.available()) {             // if there's bytes to read from the client,
+                char c = client.read();             // read a byte, then
+                Serial.write(c);                    // print it out the serial monitor
+                if (c == '\n') {                    // if the byte is a newline character
 
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length() == 0) {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println();
 
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn on the Lantern.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn off the Lantenrn.<br>");
+                        // the content of the HTTP response follows the header:
+                        client.print("Click <a href=\"/H\">here</a> to turn on the Lantern.<br>");
+                        client.print("Click <a href=\"/L\">here</a> to turn off the Lantenrn.<br>");
 
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+                        // The HTTP response ends with another blank line:
+                        client.println();
+                        // break out of the while loop:
+                        break;
+                } else {    // if you got a newline, then clear currentLine:
+                    currentLine = "";
+                }
+                } else if (c != '\r') {  // if you got anything else but a carriage return character,
+                    currentLine += c;      // add it to the end of the currentLine
+                }
+
+                // Check to see if the client request was "GET /H" or "GET /L":
+                if (currentLine.endsWith("GET /H")) {
+                      lantern_on();               // GET /H turns the LED on
+                }
+                if (currentLine.endsWith("GET /L")) {
+                    lantern_off();                // GET /L turns the LED off
+                }
+            }
         }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          lantern_on();               // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          lantern_off();                // GET /L turns the LED off
-        }
-      }
+        // close the connection:
+        client.stop();
+        Serial.println("Client Disconnected.");
     }
-    // close the connection:
-    client.stop();
-    Serial.println("Client Disconnected.");
-  }
 }
